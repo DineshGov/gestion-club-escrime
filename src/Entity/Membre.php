5bf97a584e03a813efddb24ba4dbff20dfd7658f
Slimanes <slimane_kouba@yahoo.fr>
2019-07-04 21:39:59 +0200

    API platform added

--- src/Entity/Membre.php
+++ src/Entity/Membre.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -11,6 +12,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MembreRepository")
+ * @ApiResource
  */
 class Membre
 {
